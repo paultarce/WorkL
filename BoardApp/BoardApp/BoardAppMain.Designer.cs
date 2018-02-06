252a38be72ea8ccae9fd1638b1d1184ed1356468
paultarce <paultarce@gmail.com>
2018-02-06 02:08:50 +0200

    06.02 First Commit
    And off we go!

---
+++ BoardApp/BoardApp/BoardAppMain.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace BoardApp
+{
+    partial class BoardAppMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnPlay = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cbAttachedCameras = new System.Windows.Forms.ComboBox();
+            this.cbSupportedModes = new System.Windows.Forms.ComboBox();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnPlay
+            // 
+            this.btnPlay.Location = new System.Drawing.Point(106, 301);
+            this.btnPlay.Name = "btnPlay";
+            this.btnPlay.Size = new System.Drawing.Size(75, 23);
+            this.btnPlay.TabIndex = 0;
+            this.btnPlay.Text = "Play";
+            this.btnPlay.UseVisualStyleBackColor = true;
+            this.btnPlay.Click += new System.EventHandler(this.btnPlay_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(250, 301);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 1;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Supported Cameras";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(26, 98);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(91, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Supported Modes";
+            // 
+            // cbAttachedCameras
+            // 
+            this.cbAttachedCameras.FormattingEnabled = true;
+            this.cbAttachedCameras.Location = new System.Drawing.Point(29, 51);
+            this.cbAttachedCameras.Name = "cbAttachedCameras";
+            this.cbAttachedCameras.Size = new System.Drawing.Size(172, 21);
+            this.cbAttachedCameras.TabIndex = 4;
+            // 
+            // cbSupportedModes
+            // 
+            this.cbSupportedModes.FormattingEnabled = true;
+            this.cbSupportedModes.Location = new System.Drawing.Point(29, 114);
+            this.cbSupportedModes.Name = "cbSupportedModes";
+            this.cbSupportedModes.Size = new System.Drawing.Size(172, 21);
+            this.cbSupportedModes.TabIndex = 5;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.pictureBox1.Location = new System.Drawing.Point(408, 35);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(322, 289);
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // BoardAppMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(766, 369);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.cbSupportedModes);
+            this.Controls.Add(this.cbAttachedCameras);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnPlay);
+            this.Name = "BoardAppMain";
+            this.Text = "BoardAppMain";
+            this.Load += new System.EventHandler(this.BoardAppMain_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnPlay;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cbAttachedCameras;
+        private System.Windows.Forms.ComboBox cbSupportedModes;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
---
+++ BoardApp/BoardApp/BoardAppMain.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AForge.Video;
+using AForge.Video.DirectShow;
+using AForge;
+
+namespace BoardApp
+{
+    public partial class BoardAppMain : Form
+    {
+
+        private FilterInfoCollection VideoCaptureDevices;
+        private VideoCaptureDevice FinalVideo;
+
+        public BoardAppMain()
+        {
+            InitializeComponent();
+
+            #region  Get attached cameras
+            VideoCaptureDevices = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+
+            foreach(FilterInfo VideoCaptureDevice in VideoCaptureDevices)
+            {
+                cbAttachedCameras.Items.Add(VideoCaptureDevice.Name);
+            }
+            cbAttachedCameras.SelectedIndex = 0;
+            #endregion
+
+            #region Get supported modes
+            FinalVideo = new VideoCaptureDevice(VideoCaptureDevices[cbAttachedCameras.SelectedIndex].MonikerString);
+            foreach(var capability in FinalVideo.VideoCapabilities)
+            {
+                cbSupportedModes.Items.Add(capability.FrameSize.ToString() + ":" + capability.MaximumFrameRate.ToString() + ":" + capability.BitCount.ToString());
+
+            }
+
+            cbSupportedModes.SelectedIndex = 0;
+
+            #endregion
+            FinalVideo.NewFrame += new NewFrameEventHandler(FinalVideo_NewFrame);
+
+        }
+
+        private void FinalVideo_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            if(pictureBox1.Image != null)
+            {
+                pictureBox1.Image.Dispose();
+            }
+            Bitmap tempBitmap = (Bitmap)eventArgs.Frame.Clone();
+            pictureBox1.Image = tempBitmap;
+        }
+
+        private void BoardAppMain_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnPlay_Click(object sender, EventArgs e)
+        {
+            FinalVideo.VideoResolution = FinalVideo.VideoCapabilities[cbSupportedModes.SelectedIndex];
+
+            this.Width = FinalVideo.VideoResolution.FrameSize.Height + 70;
+            this.Height = FinalVideo.VideoResolution.FrameSize.Height + 70;
+
+            this.pictureBox1.Width = FinalVideo.VideoResolution.FrameSize.Width;
+            this.pictureBox1.Height = FinalVideo.VideoResolution.FrameSize.Height;
+            FinalVideo.Start();
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            FinalVideo.Stop();
+        }
+    }
+}
